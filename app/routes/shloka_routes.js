e94a2e1a27788e74935da517cee292bbce05c400
Pooja Prakash <pooja.prakash@yasco.in>
2021-03-06 21:28:04 +0530

    BE for kosha edits

--- app/routes/shloka_routes.js
+++ app/routes/shloka_routes.js
@@ -1,4 +1,7 @@
 const { response } = require("express");
+const fs = require('fs');
+var eventHandler = require('events')
+const emitter = new eventHandler();
 
 module.exports = function(app, db) {
     app.post('/shloka', (req, res) => {
@@ -24,4 +27,41 @@ module.exports = function(app, db) {
             console.log('All done!');
         });
     });
+    let data = undefined;
+    app.get('/dhAtu', (req, res) => {
+        let sql = `SELECT dhAtu, dhAtu_num, dhAtu_gaNa from tinkrit`;
+        if(!! req.query.edit ) {
+            sql = `SELECT * from tinkrit where dhAtu = '` + req.query.edit + ` '`;
+        }
+        console.log(sql);
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            data = rows;
+            console.log('set the data');
+            emitter.emit('done', res)
+        });
+        // close the database connection
+        // db.close();
+    });
+    app.post('/dhAtu', (req, res) => {
+        const path = 'test.txt';    
+        let buffer = new Buffer(req.body);
+        fs.open(path, 'w', function(err, fd) {
+            if (err) {
+                throw 'error opening file: ' + err;
+            }
+        
+            fs.write(fd, buffer, 0, buffer.length, null, function(err) {
+                if (err) throw 'error writing file: ' + err;
+                fs.close(fd, function() {
+                    console.log('file written');
+                })
+            });
+        });
+    });
+    emitter.on('done', (res) => {
+        res.send(data);
+    })
 }
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyParser     = require('body-parser');
 const MongoClient    = require('mongodb').MongoClient;
-const db             = require('./config/db');
+// const db             = require('./config/db');
 const cors = require('cors');
+
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('./db/test.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the test database.');
+  });
 
 const port = 8080;
 const app            = express();
@@ -10,7 +19,8 @@ app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-MongoClient.connect(db.url, { useNewUrlParser: true,  useUnifiedTopology: true }, (err, database) => {  if (err) return console.log(err);     
-    const dt = database.db("texts").collection('ramayana');
-    require('./app/routes')(app, dt);app.listen(port, () => {  console.log('We are live on ' + port);});
-});
+// MongoClient.connect(db.url, { useNewUrlParser: true,  useUnifiedTopology: true }, (err, database) => {  if (err) return console.log(err);     
+//     // const dt = database.db("texts").collection('ramayana');
+//     require('./app/routes')(app, dt);app.listen(port, () => {  console.log('We are live on ' + port);});
+// });
+require('./app/routes')(app, db);app.listen(port, () => {  console.log('We are live on ' + port);});
